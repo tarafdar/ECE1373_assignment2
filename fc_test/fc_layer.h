d5631805235cf6143a04cc46d733860d9be540ad
Ubuntu <ubuntu@vivado-test.lxd>
2018-02-14 19:19:31 +0000

    fixed dimensions in header

--- fc_test/fc_layer.h
+++ fc_test/fc_layer.h
@@ -2,8 +2,8 @@
 #define _FC_LAYER_H
 
 #define MAX_BATCH 10
-#define MAX_INPUT_SIZE 1024
-#define MAX_OUTPUT_SIZE 1024
+#define MAX_INPUT_SIZE 4096 
+#define MAX_OUTPUT_SIZE 4096
 
 void fc_layer(
               float * mem,
