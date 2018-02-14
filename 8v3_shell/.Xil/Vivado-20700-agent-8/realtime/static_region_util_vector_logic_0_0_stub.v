7bfb0974fa5a3ce1ce3092ed4118b8d109f21020
root <root@vivado-test.lxd>
2018-02-14 03:01:22 +0000

    removed hidden xil folder

--- 8v3_shell/.Xil/Vivado-20700-agent-8/realtime/static_region_util_vector_logic_0_0_stub.v
+++
@@ -1,9 +0,0 @@
-// Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
-
-// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
-// Please paste the declaration into a Verilog source file or add the file as an additional source.
-(* X_CORE_INFO = "util_vector_logic_v2_0_1_util_vector_logic,Vivado 2017.2" *)
-module static_region_util_vector_logic_0_0(Op1, Res);
-  input [0:0]Op1;
-  output [0:0]Res;
-endmodule
