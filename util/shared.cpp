b41d0708fb539f9938f8c8fd0a37c1e4f2d21d5b
charleslo <charles@charleslo.net>
2018-02-15 22:36:10 +0000

    Copy over modified shared files

--- util/shared.cpp
+++ util/shared.cpp
@@ -1,10 +1,77 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 
 #include "shared.h"
 
 using namespace std;
+
+std::map<std::string, int> readParams(const std::string fname)
+{
+  std::map<std::string, int> params;
+  ifstream in_file(fname.c_str(), ios::in);
+  std::string key, svalue;
+  int ivalue;
+
+  // Warning: don't do much error checking
+  while (in_file >> key)
+  {
+    if (!key.compare("type")) {
+      in_file >> svalue;
+      if (!svalue.compare("Convolution"))
+        params[key] = Convolution;
+      else if (!svalue.compare("InnerProduct"))
+        params[key] = InnerProduct;
+      else if (!svalue.compare("Pooling"))
+        params[key] = Pooling;
+      else {
+        cerr << "Invalid Layer Type!\n";
+        return std::map<std::string, int>();
+      }
+    } else if (!key.compare("name")) { 
+      in_file >> svalue;
+      std::cout << "Parsing " << svalue << std::endl;
+    }
+    else {
+       in_file >> ivalue;
+       params[key] = ivalue;
+    }
+  }
+  in_file.close();
+  std::cout << "DONE\n";
+  return params;
+}
+int readRawFile(const string fname,
+                              float *& fptr,
+                              const int read_alloc,
+                              const int max_alloc)
+{
+  int retval = 0;
+
+  std::cout << "Reading: " << fname << " size: " << read_alloc << std::endl;
+  ifstream in_file(fname.c_str(), ios::in | ios::binary);
+  if (in_file.is_open())
+  {
+    fptr = new float[max_alloc];
+    if (read_alloc <= max_alloc) {
+      if (!in_file.read(reinterpret_cast<char*>(&fptr[0]), sizeof(float)*read_alloc))
+      {
+    	  cout << "Read Error" << endl;
+          retval = 1;
+      }
+    } else {
+      cerr << "Desired dimensions too large: " << read_alloc << " > " << max_alloc << "\n";
+      retval = 1;
+    }
+    in_file.close();
+  }
+  else
+    cerr << "Couldn't open file: " << fname << endl;
+
+  if (retval) delete [] fptr;
+  return retval;
+}
 
 std::vector <int> readFile(const string fname,
                      float *& fptr,
@@ -27,21 +94,48 @@ std::vector <int> readFile(const string fname,
                  sizeof(int)*read_dims);
 
     // Read the array
+    // Use a hack to read dma data:
     int read_alloc = 1;
-    for (int i = 0; i < read_dims; i++){
-      read_alloc *= read_sizes[i];
-      read_sizes_vec.push_back(read_sizes[i]);
+    if (read_dims == 9) {
+    	cout << "Reading DMA Input" << endl;
+    	read_alloc = read_sizes[0]*read_sizes[1] *   // weights
+    			     read_sizes[2]*read_sizes[3] +
+    			     read_sizes[4] +                 // biases
+					 read_sizes[5]*read_sizes[6] *   // inputs
+					 read_sizes[7]*read_sizes[8];
+    	for (int i = 0; i < read_dims; i++)
+    	  read_sizes_vec.push_back(read_sizes[i]);
     }
-
-    if (read_alloc <= max_alloc)
-      in_file.read(reinterpret_cast<char*>(&fptr[0]), sizeof(float)*read_alloc);
     else
     {
-      cerr << "Desired dimensions too large\n";
+    	cout << "READING NORMAL INPUT" << endl;
+      read_alloc = 1;
+      for (int i = 0; i < read_dims; i++){
+    	cout << i << " = " << read_sizes[i] << endl;
+        read_alloc *= read_sizes[i];
+        read_sizes_vec.push_back(read_sizes[i]);
+      }
+    }
+    cout << "READ ALLOC " << read_alloc << " for " << read_dims << endl;
+    if (read_alloc <= max_alloc) {
+      if (!in_file.read(reinterpret_cast<char*>(&fptr[0]), sizeof(float)*read_alloc))
+      {
+    	  cout << "Read Error" << endl;
+    	  delete [] read_sizes;
+    	  delete [] fptr;
+    	  read_sizes = NULL;
+      }
+    }
+    else
+    {
+      cerr << "Desired dimensions too large: " << read_alloc << " > " << max_alloc << "\n";
       delete [] read_sizes;
       delete [] fptr;
       read_sizes = NULL;
     }
+
+    for (int i = 0; i < 10; i++)
+    	cout << fptr[i] << endl;
     in_file.close();
   }
   else
--- util/shared.h
+++ util/shared.h
@@ -3,11 +3,17 @@
 
 #include <string>
 #include <vector>
+#include <map>
 
 #define MAX_BATCH 10
 #define NUM_BIAS_DIMENSIONS 1
 std::vector<int> readFile(const std::string fname,
                           float *& fptr,
                           const int max_alloc);
-
+int readRawFile(const std::string fname,
+                float *& fptr,
+                const int read_alloc,
+                const int max_alloc);
+std::map<std::string, int> readParams(const std::string fname);
+enum LayerTypes {Convolution, Pooling, InnerProduct};
 #endif
