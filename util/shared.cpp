2233e7999445cabfce342d9d7ad129c75e9a8ac2
charleslo <charles@charleslo.net>
2018-02-16 18:50:13 +0000

    migrated common hardware functions to shared

--- util/shared.cpp
+++ util/shared.cpp
@@ -2,8 +2,24 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <stdint.h>
+#include <assert.h>
 
 #include "shared.h"
+
+/* ltoh: little to host */
+/* htol: little to host */
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+#  define ltohl(x)       (x)
+#  define ltohs(x)       (x)
+#  define htoll(x)       (x)
+#  define htols(x)       (x)
+#elif __BYTE_ORDER == __BIG_ENDIAN
+#  define ltohl(x)     __bswap_32(x)
+#  define ltohs(x)     __bswap_16(x)
+#  define htoll(x)     __bswap_32(x)
+#  define htols(x)     __bswap_16(x)
+#endif
 
 using namespace std;
 
@@ -143,4 +159,35 @@ std::vector <int> readFile(const string fname,
 
   delete [] read_sizes;
   return read_sizes_vec;
+}
+
+void write_int(volatile void* map_base, int offset, int value)
+{
+  volatile void* virt_addr = (volatile void*)((char*)map_base + offset); 
+  *((uint32_t *) virt_addr) = htoll(value);
+}
+int read_int(volatile void* map_base, int offset)
+{
+  volatile void* virt_addr = (volatile void*)((char*)map_base + offset); 
+  return ltohl(*((uint32_t *) virt_addr));
+}
+
+void timespec_sub(struct timespec *t1, const struct timespec *t2)
+{
+  assert(t1->tv_nsec >= 0);
+  assert(t1->tv_nsec < 1000000000);
+  assert(t2->tv_nsec >= 0);
+  assert(t2->tv_nsec < 1000000000);
+  t1->tv_sec -= t2->tv_sec;
+  t1->tv_nsec -= t2->tv_nsec;
+  if (t1->tv_nsec >= 1000000000)
+  {
+    t1->tv_sec++;
+    t1->tv_nsec -= 1000000000;
+  }
+  else if (t1->tv_nsec < 0)
+  {
+    t1->tv_sec--;
+    t1->tv_nsec += 1000000000;
+  }
 }
