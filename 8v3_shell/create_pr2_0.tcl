15b58d60fb0a5865fd9edfe96781e5344e3387cd
savi <tarafdar12@gmail.com>
2018-02-20 04:19:52 +0000

    fixed project name in pr script and fixed inconsistent naming in nn_params

--- 8v3_shell/create_pr2_0.tcl
+++ 8v3_shell/create_pr2_0.tcl
@@ -1,4 +1,4 @@
-set projName pr_region_test
+set projName pr_region_test_proj
 create_project pr_region 8v3_shell/$projName -part xcvu095-ffvc1517-2-e
 set_property  ip_repo_paths  {8v3_shell/ocl_ips hls_proj/conv_proj/solution1/impl/ip hls_proj/fc_proj/solution1/impl/ip } [current_project]
 update_ip_catalog
